f35132dceb398d6fe9be9941bf0221245f063f8f
Antek Krasiński <antekrasinski@gmail.com>
2023-02-20 13:45:08 +0100

    init

---
+++ Controllers/GradesController.cs
@@ -0,0 +1,112 @@
+﻿using Gradebook.Model;
+using Gradebook.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gradebook.Controllers
+{
+    [ApiController]
+    [Route("grades")]
+    public class GradesController : ControllerBase
+    {
+        private readonly GradesRepository _gradesRepository;
+
+        public GradesController()
+        {
+            _gradesRepository = new GradesRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<GradeRecord> GetGradeRecords()
+        {
+            var gradeRecords = _gradesRepository.GetItems();
+            return gradeRecords;    
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<GradeRecord> GetGradeRecord(Guid id)
+        {
+            var gradeRecord = _gradesRepository.GetGradeRecord(id);
+            if (gradeRecord == null)
+            {
+                return NotFound();
+            }
+            return Ok(gradeRecord);
+        }
+/*
+        // GET: GradesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GradesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GradesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GradesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GradesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GradesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GradesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GradesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }*/
+    }
+}
---
+++ Repositories/GradesRepository.cs
@@ -0,0 +1,24 @@
+﻿using Gradebook.Model;
+
+namespace Gradebook.Repositories
+{
+    public class GradesRepository
+    {
+        private readonly List<GradeRecord> gradeRecords = new()
+        {
+            new GradeRecord {GradeRecordId = Guid.NewGuid(), CreatedDate = DateTime.Now, Description = "Exam", Grade = 4.5, Subject = "Math" },
+            new GradeRecord {GradeRecordId = Guid.NewGuid(), CreatedDate = DateTime.Now, Description = "Homework", Grade = 4, Subject = "Math" },
+            new GradeRecord {GradeRecordId = Guid.NewGuid(), CreatedDate = DateTime.Now, Description = "Group project", Grade = 5, Subject = "Math" }
+        };
+
+        public IEnumerable<GradeRecord> GetItems()
+        {
+            return gradeRecords;
+        }
+
+        public GradeRecord GetGradeRecord(Guid gradeRecordId)
+        {
+            return gradeRecords.SingleOrDefault(gradeRecord => gradeRecord.GradeRecordId == gradeRecordId);
+        }
+    }
+}
